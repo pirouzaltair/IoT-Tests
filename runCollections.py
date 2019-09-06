062d5b834b265747426b8d13e66876397ec8a720
Pirouz Mehmandoost <pirouzm@Alairs-MacBook-Pro.local>
2019-09-06 11:08:16 -0700

    add python script to run collections.

---
+++ runCollections.py
@@ -0,0 +1,19 @@
+import os
+
+# start in the directory this script is run from
+folder = 'collections'
+extension = '.json'
+
+def files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+        	if file.endswith(extension):
+	            yield os.path.join(path, file)
+
+for file in files(folder):
+    # print 'processing {}'.format(file)
+    # use quotes so the shell can handle spaces in filenames
+    cmd = 'newman run "{}" -e environments/IotFullTest.postman_environment.json'.format(file)
+    os.system(cmd)
+
+print 'done!'
