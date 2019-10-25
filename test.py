deca32dab5a914a25165e0a89f9cf6922b9c6964
Pirouz Mehmandoost <51347314+pirouzaltair@users.noreply.github.com>
2019-10-25 12:04:37 -0700

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ extension = '.json'
 os.system('npm install -g npm')
 os.system('npm install -g newman')
 os.system('npm install -g newman-reporter-html')
-os.system('npm install -g newman-reporter-htmlextra')
-cmd = 'newman run collections/UsageDataAPIlargedateranges.postman_collection.json  -e environments/IoTFullTest.postman_environment.json -r htmlextra --reporter-htmlextra-logs'.format(file)
+cmd = 'newman run collections/UsageDataAPIlargedateranges.postman_collection.json  -e environments/IoTFullTest.postman_environment.json -r html'.format(file)
 os.system(cmd)
 print 'done!'
