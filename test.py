eb1f320226b38714c07ba05118bcf885e7725cb8
Pirouz Mehmandoost <51347314+pirouzaltair@users.noreply.github.com>
2019-10-25 12:00:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,6 @@ os.system('npm install -g npm')
 os.system('npm install -g newman')
 os.system('npm install -g newman-reporter-html')
 os.system('npm install -g newman-reporter-htmlextra')
-
-
-
-    # print 'processing {}'.format(file)
-    # use quotes so the shell can handle spaces in filenames
-    cmd = 'newman run -e environments/IoTFullTest.postman_environment.json collections/DeviceEventData.postman_collection.json -r htmlextra --reporter-htmlextra-logs'.format(file)
-    os.system(cmd)
-
+cmd = 'newman run collections/UsageDataAPIlargedateranges.postman_collection.json  -e environments/IoTFullTest.postman_environment.json -r htmlextra --reporter-htmlextra-logs'.format(file)
+os.system(cmd)
 print 'done!'
